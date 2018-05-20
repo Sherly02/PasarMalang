72825a5d37b1b7eafc86375f650996b2001e6919
Rifky Sultan Karisma A <rifky_andriansyah_25rpl@student.smktelkom-mlg.sch.id>
2018-05-20 20:23:34 +0700

    + Membuat variable constant String TAG untuk getSimpleName() method + Membuat variable private RequestQueue + membuat variable static MyApplication

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1103033/pasarmalang/application/MyApplication.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1103033/pasarmalang/application/MyApplication.java
@@ -2,9 +2,17 @@ package id.sch.smktelkom_mlg.afinal.xirpl1103033.pasarmalang.application;
 
 import android.app.Application;
 
+import com.android.volley.RequestQueue;
+
 /**
  * Created by batuh on 20/05/2018.
  */
 
 public class MyApplication extends Application {
+
+    public static final String TAG = MyApplication.class
+            .getSimpleName();
+    private static MyApplication mInstance;
+    private RequestQueue mRequestQueue;
+
 }
