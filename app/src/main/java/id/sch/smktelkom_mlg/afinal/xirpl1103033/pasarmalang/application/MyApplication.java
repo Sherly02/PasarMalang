890a164178a04480e5c3cc9cef30dcf73682ffcf
Rifky Sultan Karisma A <rifky_andriansyah_25rpl@student.smktelkom-mlg.sch.id>
2018-05-20 20:20:50 +0700

    + Menambahkan extends Application

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1103033/pasarmalang/application/MyApplication.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1103033/pasarmalang/application/MyApplication.java
@@ -1,8 +1,10 @@
 package id.sch.smktelkom_mlg.afinal.xirpl1103033.pasarmalang.application;
 
+import android.app.Application;
+
 /**
  * Created by batuh on 20/05/2018.
  */
 
-public class MyApplication {
+public class MyApplication extends Application {
 }
