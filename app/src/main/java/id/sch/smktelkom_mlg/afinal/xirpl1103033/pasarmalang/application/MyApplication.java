f3d24bc8057530e5a3ac9e0368444c76b4efdeaf
Rifky Sultan Karisma A <rifky_andriansyah_25rpl@student.smktelkom-mlg.sch.id>
2018-05-20 20:28:38 +0700

    + Membuat public void untuk menambahkan Request, dengan menyetel tag jika kosong

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1103033/pasarmalang/application/MyApplication.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1103033/pasarmalang/application/MyApplication.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.afinal.xirpl1103033.pasarmalang.application;
 
 import android.app.Application;
+import android.text.TextUtils;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
@@ -32,6 +34,11 @@ public class MyApplication extends Application {
         }
 
         return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
     }
 
 }
