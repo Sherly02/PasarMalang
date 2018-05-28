067c439503fd385daa9ddc590b741cb89e996f96
Rifky Sultan Karisma A <rifky_andriansyah_25rpl@student.smktelkom-mlg.sch.id>
2018-05-28 13:22:49 +0700

    + Membuat set dan get variable

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1103033/pasarmalang/model/Product.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1103033/pasarmalang/model/Product.java
@@ -12,5 +12,43 @@ public class Product {
     String location_product;
     String unit;
 
+    public String getName_product() {
+        return name_product;
+    }
 
+    public void setName_product(String name_product) {
+        this.name_product = name_product;
+    }
+
+    public String getPrice_today() {
+        return price_today;
+    }
+
+    public void setPrice_today(String price_today) {
+        this.price_today = price_today;
+    }
+
+    public String getPrice_yerterday() {
+        return price_yerterday;
+    }
+
+    public void setPrice_yerterday(String price_yerterday) {
+        this.price_yerterday = price_yerterday;
+    }
+
+    public String getLocation_product() {
+        return location_product;
+    }
+
+    public void setLocation_product(String location_product) {
+        this.location_product = location_product;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 }
