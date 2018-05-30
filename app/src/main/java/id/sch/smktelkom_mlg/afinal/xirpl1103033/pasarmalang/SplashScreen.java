112d4ff041da4f3f295a4414670d6fd234af3600
Sherly <sherly_mauren_25rpl@student.smktelkom-mlg.sch.id>
2018-05-30 21:24:51 +0700

    memperbaiki SplashScreen.java

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1103033/pasarmalang/SplashScreen.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1103033/pasarmalang/SplashScreen.java
@@ -8,7 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class Splash_screen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
